85502e3effe1beaf5af66837f1f657da81ad04e9
Arūnas Smaliukas <arunas@smala.lt>
2025-05-08 08:52:47 +0300

    Update integrations.lumbering.service.ts

--- services/integrations.lumbering.service.ts
+++ services/integrations.lumbering.service.ts
@@ -21,7 +21,7 @@ import { IntegrationsMixin, IntegrationStats } from '../mixins/integrations.mixi
   crons: [
     {
       name: 'integrationsLumbering',
-      cronTime: '0 4 * * *',
+      cronTime: '0 9 * * *',
       timeZone: 'Europe/Vilnius',
       async onTick() {
         await this.call('integrations.lumbering.getData', {
